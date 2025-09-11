6c882af9138cadc8479c43094ea23ab585c9412b
merms2002 <indense7@GMail.com>
2025-09-11 07:11:43 +0300

    Delete plugins

--- plugins/_block-check.js
+++
@@ -1,7 +0,0 @@
-export async function before(m, { command }) {
-    global.db.data.blockedCommands = global.db.data.blockedCommands || []
-
-    if (global.db.data.blockedCommands.includes(command)) {
-        throw `❌ الأمر *${command}* مقيد حاليًا.`
-    }
-}
