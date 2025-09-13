c11356af6f3b25071d580f3dd009b85a7dbb8107
merms2002 <indense7@GMail.com>
2025-09-13 12:18:13 +0300

    Upload decompressed file plugins/الاوامر-AI.js

---
+++ plugins/الاوامر-AI.js
@@ -0,0 +1,40 @@
+import pkg from '@whiskeysockets/baileys';
+const { prepareWAMessageMedia } = pkg;
+
+const handler = async (m, { conn }) => {
+    await conn.sendMessage(m.chat, { react: { text: '👨‍💻', key: m.key } });
+
+    const harley = 'https://files.catbox.moe/9qx3sk.jpg';
+
+    // تعريف المتغيرات المفقودة (أنت عدل القيم حسب نظامك)
+    const taguser = m.sender.split('@')[0];
+    const exp = 100;
+    const role = 'مبتدئ';
+    const limit = 10;
+
+    const media = await prepareWAMessageMedia({ image: { url: harley } }, { upload: conn.waUploadToServer });
+
+    await conn.sendMessage(m.chat, {
+        image: { url: harley },
+        caption: `*◡̈⃝🚩 ╎${taguser} 」*
+~*⊹‏⊱≼━━━⌬〔🏮〕⌬━━━≽⊰⊹*
+> *الــــذكـــاء الـإصــطـنــاعــي ˼🤖˹↶*
+~*⊹‏⊱≼━━━⌬〔🏮〕⌬━━━≽⊰⊹*
+*˼🔔˹╎كريس 」*
+*˼☔˹╎ســيـــلـــيــا 」*
+*˼📖˹╎شــادو 」*
+*˼🤖˹╎غــوجــو 」*
+*˼☠️˹╎بـروك 」*
+*⊹‏⊱≼━━━⌬〔🏮〕⌬━━━≽⊰⊹*`,
+        mentions: [m.sender],
+        footer: 'Edward Bot',
+        
+        headerType: 4 // نوع الرسالة مع صورة
+    }, { quoted: m });
+};
+
+handler.help = ['اوامر'];
+handler.tags = ['main'];
+handler.command = ['م4'];
+
+export default handler;
